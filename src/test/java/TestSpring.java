e7b34089b7688685f8163db4b5d24660eb493fb9
BliznyukS <bliznyuk.sergey@gmail.com>
2020-04-02 18:02:51 +0300

    Bean -> Init and Destroy methods

--- src/test/java/TestSpring.java
+++ src/test/java/TestSpring.java
@@ -1,4 +1,5 @@
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import ru.alishev.springcourse.ClassicalMusic;
 import ru.alishev.springcourse.Music;
 import ru.alishev.springcourse.MusicPlayer;
 
@@ -6,22 +7,26 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        ClassicalMusic classicalMusic = context.getBean("musicBean", ClassicalMusic.class);
 
-        boolean comparison = firstMusicPlayer == secondMusicPlayer;
-        System.out.println(firstMusicPlayer);
-        System.out.println(secondMusicPlayer);
-        System.out.println(comparison);
+        System.out.println(classicalMusic.getSong());
 
-        boolean comparisonEquals = firstMusicPlayer.equals(secondMusicPlayer);
-        System.out.println(firstMusicPlayer);
-        System.out.println(secondMusicPlayer);
-        System.out.println(comparisonEquals);
-
-        firstMusicPlayer.setVolume(10);
-        System.out.println(firstMusicPlayer.getVolume());
-        System.out.println(secondMusicPlayer.getVolume());
+//        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//
+//        boolean comparison = firstMusicPlayer == secondMusicPlayer;
+//        System.out.println(firstMusicPlayer);
+//        System.out.println(secondMusicPlayer);
+//        System.out.println(comparison);
+//
+//        boolean comparisonEquals = firstMusicPlayer.equals(secondMusicPlayer);
+//        System.out.println(firstMusicPlayer);
+//        System.out.println(secondMusicPlayer);
+//        System.out.println(comparisonEquals);
+//
+//        firstMusicPlayer.setVolume(10);
+//        System.out.println(firstMusicPlayer.getVolume());
+//        System.out.println(secondMusicPlayer.getVolume());
 
         //System.out.println(musicPlayer.getName());
         //System.out.println(musicPlayer.getVolume());
