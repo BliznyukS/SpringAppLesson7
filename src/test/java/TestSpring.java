b01b568ee098f7dded211cdad4122b4d9d7bd7fe
BliznyukS <bliznyuk.sergey@gmail.com>
2020-04-02 17:40:35 +0300

    Initial commit

---
+++ src/test/java/TestSpring.java
@@ -0,0 +1,32 @@
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import ru.alishev.springcourse.Music;
+import ru.alishev.springcourse.MusicPlayer;
+
+public class TestSpring {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+
+        boolean comparison = firstMusicPlayer == secondMusicPlayer;
+        System.out.println(firstMusicPlayer);
+        System.out.println(secondMusicPlayer);
+        System.out.println(comparison);
+
+        boolean comparisonEquals = firstMusicPlayer.equals(secondMusicPlayer);
+        System.out.println(firstMusicPlayer);
+        System.out.println(secondMusicPlayer);
+        System.out.println(comparisonEquals);
+
+        firstMusicPlayer.setVolume(10);
+        System.out.println(firstMusicPlayer.getVolume());
+        System.out.println(secondMusicPlayer.getVolume());
+
+        //System.out.println(musicPlayer.getName());
+        //System.out.println(musicPlayer.getVolume());
+
+        context.close();
+    }
+
+}
